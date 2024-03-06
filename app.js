449240c97b1060932d99ce9227233e684a4b8f8d
Mireillerita <mireillendahimana@gmail.com>
2024-03-06 06:18:18 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const hamburger = document.querySelector(#hamburger)
+const hamburger = document.querySelector(#menu)
+
+hamburger.addEventListerner("click", ()=>{
+    menubar.classList.toggle("hidden")
+})
